97a0d860d4b28abdefb86453d58c5543797670d3
Nana_Mizuki <fallakiakino@gmail.com>
2019-07-14 12:06:38 +0900

    Chopter3 途中

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller{
+	public function index(){
+		$data = ['one','two','three','four','five'];
+		return view('hello.index',['data'=>$data]);
+	}
+
+	public function post(Request $request){
+		return view('hello.index',['msg'=>$request->msg]);
+	}
+}	
