dc4c707c3dff01dfd54a4e6e94ce851659fb32db
Mizuki <Mizuki@no_replay>
2019-07-17 01:13:07 +0900

    0717 -p120

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,9 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+        
+        'helo' =>['helo' => \App\Http\Middleware\HelloMiddleware::class,
+        ],
     ];
 
     /**
@@ -52,7 +55,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'hello' => \App\Http\Middleware\HelloMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
@@ -61,7 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
 
 //use App\Http\Middleware\HelloMiddleware;
 
-Route::get('hello', 'HelloController@index')
-    ->middleware(HelloMiddleware::class);
+Route::get('hello', 'HelloController@index');
 
 Route::post('hello', 'HelloController@post');
