ab12418215e766abbfc4ca678a390d74586e3c47
Mizuki <Mizuki@no_replay>
2019-07-20 14:53:06 +0900

    p153

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -4,16 +4,17 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-// use Validator;
-// use App\Http\validators\HelloValidator;
+use Validator;
+use App\Http\validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
 {
  public function boot()
  {
-     $validator = $this ->app['validator'];
-     $validator->resolver(function($translator, $data, $rules, $messages){
-         return new HelloValidator($translator, $data, $rules, $messages);
-     });
+    Validator::extend('hello',function($attribute, $value, $paraments, $validator){
+            return $value%2==0;
+    });
+
+     
  }
 }
